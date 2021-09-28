a73c80d996f7b620c4103700cc491c0049b19d7e
Pablo Lopez-Jamar <pablo.lopez.jamar@gmail.com>
2021-09-28 16:05:55 -0700

    Added search command

--- htmls_to_datasette/cli.py
+++ htmls_to_datasette/cli.py
@@ -1,18 +1,26 @@
 import hashlib
 import pathlib
-from rich.progress import track
-from rich import print
-from rich.traceback import install
+from dataclasses import dataclass
 from datetime import datetime
 
 import click
 import html2text
 import sqlite_utils
+from rich import print
+from rich.progress import track
+from rich.traceback import install
 
 install(show_locals=False)
 
-
+DATABASE_NAME = "htmlstore.db"
 FILES_TABLE = "files"
+ACCEPTABLE_HTML_EXTENSIONS = [".html", ".htm"]
+
+
+@dataclass
+class IndexStats:
+    already_indexed: int = 0
+    indexed: int = 0
 
 
 class TableColumns(object):
@@ -23,6 +31,51 @@ class TableColumns(object):
     ADDED = "added"
     PLAINTEXT_CONTENT = "plaintext-content"
     CONTENT = "content"
+
+
+def index_dir(
+    directory: pathlib.Path,
+    db: sqlite_utils.Database,
+    store_binary: bool,
+    stats: IndexStats,
+):
+    files_to_process = [
+        file
+        for file in directory.iterdir()
+        if file.is_file() and file.suffix.lower() in ACCEPTABLE_HTML_EXTENSIONS
+    ]
+
+    for file in track(
+        files_to_process, description=f'Indexing files on "{directory}"...'
+    ):
+        path = str(file)
+        previous_file_entry = next(
+            db.query(
+                f"select id from {FILES_TABLE} where {TableColumns.PATH} = :{TableColumns.PATH}",
+                {TableColumns.PATH: path},
+            ),
+            None,
+        )
+        if not previous_file_entry:
+            stats.indexed = stats.indexed + 1
+            filename = file.name
+            id_ = hashlib.md5(path.encode("UTF-8")).hexdigest()
+            size = file.stat().st_size
+            with open(file, "r") as file_handler:
+                content = file_handler.read()
+                values = {
+                    TableColumns.ID: id_,
+                    TableColumns.NAME: filename,
+                    TableColumns.SIZE: size,
+                    TableColumns.PATH: path,
+                    TableColumns.ADDED: datetime.now().strftime("%d-%m-%Y"),
+                    TableColumns.PLAINTEXT_CONTENT: html2text.html2text(content),
+                }
+                if store_binary:
+                    values[TableColumns.CONTENT] = content
+                db[FILES_TABLE].insert(values, pk="id")
+        else:
+            stats.already_indexed = stats.already_indexed + 1
 
 
 def html_file_to_text(file):
@@ -57,7 +110,7 @@ def initialize_db(db: sqlite_utils.Database):
     "--database",
     "-d",
     type=click.Path(dir_okay=False),
-    default="htmlstore.db",
+    default=DATABASE_NAME,
     help="The index database to update.",
 )
 @click.option(
@@ -75,59 +128,21 @@ def index(input_dirs: str, database: str, store_binary: bool):
     db = sqlite_utils.Database(database)
     initialize_db(db)
 
+    stats = IndexStats()
     for input_dir in input_dirs:
-        search_path = pathlib.Path(input_dir)
-        files_to_process = [
-            file
-            for file in search_path.iterdir()
-            if file.is_file() and file.suffix.lower() in [".html", ".htm"]
-        ]
+        index_dir(pathlib.Path(input_dir), db, store_binary, stats)
 
-        files_indexed = 0
-        files_not_indexed = 0
-        for file in track(
-            files_to_process, description=f'Indexing files on "{search_path}"...'
-        ):
-            path = str(file)
-            previous_file_entry = next(
-                db.query(
-                    f"select id from {FILES_TABLE} where {TableColumns.PATH} = :{TableColumns.PATH}",
-                    {TableColumns.PATH: path},
-                ),
-                None,
-            )
-            if not previous_file_entry:
-                files_indexed = files_indexed + 1
-                filename = file.name
-                id_ = hashlib.md5(path.encode("UTF-8")).hexdigest()
-                size = file.stat().st_size
-                with open(file, "r") as file_handler:
-                    content = file_handler.read()
-                    values = {
-                        TableColumns.ID: id_,
-                        TableColumns.NAME: filename,
-                        TableColumns.SIZE: size,
-                        TableColumns.PATH: path,
-                        TableColumns.ADDED: datetime.now().strftime('%d-%m-%Y'),
-                        TableColumns.PLAINTEXT_CONTENT: html2text.html2text(content),
-                    }
-                    if store_binary:
-                        values[TableColumns.CONTENT] = content
-                    db["files"].insert(values, pk="id")
-            else:
-                files_not_indexed = files_not_indexed + 1
-
-        if files_not_indexed:
-            print(f"{files_not_indexed} file(s) were already indexed!")
-        if files_indexed:
-            print(f"{files_indexed} file(s) were indexed.")
+    if stats.already_indexed:
+        print(f"{stats.already_indexed} file(s) were already indexed!")
+    if stats.indexed:
+        print(f"{stats.indexed} file(s) were indexed.")
 
 
 @click.option(
     "--database",
     "-d",
     type=click.Path(dir_okay=False),
-    default="htmlstore.db",
+    default=DATABASE_NAME,
     help="The index database to update.",
 )
 @click.option("--dry-run", is_flag=True, help="Do not perform any actions.")
@@ -171,7 +186,7 @@ def purge(database: str, dry_run: bool):
     "--database",
     "-d",
     type=click.Path(dir_okay=False),
-    default="htmlstore.db",
+    default=DATABASE_NAME,
     help="The index database to update.",
 )
 @click.option("--dry-run", is_flag=True, help="Do not perform any actions.")
@@ -194,7 +209,9 @@ def extract(output: str, database: str, dry_run: bool):
         f"select id, path from {FILES_TABLE} where content is not null"
     ):
         path = pathlib.Path(row[TableColumns.PATH])
-        if (not output_path and path.exists()) or (output_path and output_path.joinpath(path.name).exists()):
+        if (not output_path and path.exists()) or (
+            output_path and output_path.joinpath(path.name).exists()
+        ):
             warning_files_found = warning_files_found + 1
         elif not output_path and not path.parent.exists():
             files_with_parent_directory_not_found.append(row)
@@ -221,13 +238,50 @@ def extract(output: str, database: str, dry_run: bool):
         return
 
     for row in track(files_to_extract, description="Extracting files..."):
-        content = next(db.query(
-            f"select {TableColumns.CONTENT} from {FILES_TABLE} where {TableColumns.ID} = :id",
-            {"id": row[TableColumns.ID] }
-        ))[TableColumns.CONTENT]
-        path = output_path.joinpath(pathlib.Path(row[TableColumns.PATH]).name) if output_path else row[TableColumns.PATH]
-        with open(path, 'w') as file:
+        content = next(
+            db.query(
+                f"select {TableColumns.CONTENT} from {FILES_TABLE} where {TableColumns.ID} = :id",
+                {"id": row[TableColumns.ID]},
+            )
+        )[TableColumns.CONTENT]
+        path = (
+            output_path.joinpath(pathlib.Path(row[TableColumns.PATH]).name)
+            if output_path
+            else row[TableColumns.PATH]
+        )
+        with open(path, "w") as file:
             file.write(content)
+
+
+@click.argument("query", type=click.STRING, nargs=-1)
+@click.option(
+    "--database",
+    "-d",
+    type=click.Path(dir_okay=False),
+    default=DATABASE_NAME,
+    help="The index database to update.",
+)
+@click.command()
+def search(query: str, database: str):
+    """Performs a full text search over the indexed HTML files."""
+
+    print(f"Using database [bold]{database}[/bold].")
+    db = sqlite_utils.Database(database)
+    initialize_db(db)
+    full_query = " ".join(query)
+
+    entries = db.query(
+        f"select {TableColumns.ADDED}, {TableColumns.NAME}, {TableColumns.PATH}, {TableColumns.SIZE} "
+        + f"from {FILES_TABLE} where rowid in (select rowid from {FILES_TABLE}_fts where {FILES_TABLE}_fts match :q) "
+        + f"order by {TableColumns.ADDED} desc "
+        + f"limit 50",
+        {"q": full_query},
+    )
+
+    print(f"Search '{full_query}'")
+    for result in entries:
+        file = pathlib.Path(result[TableColumns.PATH]).absolute()
+        print(f"  - {file}")
 
 
 @click.group()
@@ -241,4 +295,5 @@ def cli():
     main.add_command(index)
     main.add_command(purge)
     main.add_command(extract)
+    main.add_command(search)
     main()
